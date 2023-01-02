ff8649a007bf6ffff77d5504b69e6b7d65b2c766
NichHarris <harris.nicholas1998@gmail.com>
2023-01-02 14:14:23 -0500

    Create template for nn and add db funcs for practice module

--- app/practice_module/controller.py
+++ app/practice_module/controller.py
@@ -1,14 +1,7 @@
 from config import Database
-from pymongo import errors
-
-from os import environ as env
-from dotenv import find_dotenv, load_dotenv
-
-from datetime import datetime
+from pymongo import errors, results, cursor
 import random
-
-# load the environment variables from the .env file
-load_dotenv(find_dotenv())
+from datetime import datetime
 
 # get a reference to the databases
 intera_practice_db = Database.client.intera_practice_db
@@ -19,23 +12,100 @@ try:
 except errors.CollectionInvalid as err:
     print(err)
 
-def create_word_entry(word, url):
-    word_entry = {'word': word, 'url': url, 'date_created': datetime.now()}
-    word_data.insert_one(word_entry)
+def create_word_entry(word, url, classified=True):
+    word_entry = {'word': word, 'url': url, 'classified': classified}
+    result = word_data.insert_one(word_entry)
+
+    if isinstance(result, results.InsertOneResult):
+        if result.inserted_id:
+            return (1, 'Word {word} created successfully')
+
+    return (0, 'Error creating word {word}')
+
 
 def retrieve_random_word(self):
-
     # get random word from mongodb without using aggregate
-    count = word_data.count_documents({})
+    count = word_data.count_documents({'classified': True})
     random_index = random.randint(0, count - 1)
-    word = word_data.find()[random_index]
+    word = word_data.find({'classified': True})[random_index]
 
-    return word
+    if word is None:
+        return (0, 'No classified words found', None)
+
+    return (1, 'success', word)
+
 
 def get_word_video_url(word):
     word_entry = word_data.find_one({'word': word})
 
     if word_entry:
-        return word_entry['url']
+        return (1, 'success', word_entry['url'])
     
-    return None
+    return (0, 'Unable to find word {word}', None)
+
+
+def set_classified_status(word, classified):
+    result = word_data.update_one({'word': word}, {'$set': {'classified': classified}})
+
+    if isinstance(result, results.UpdateResult):
+        if result.matched_count == 1:
+            return (1, 'Word {word} updated successfully')
+        else:
+            return (0, 'Word {word} not found')
+    
+    return (0, 'Error updating word {word}')
+
+
+def delete_word(word):
+    result = word_data.delete_one({'word': word})
+
+    if isinstance(result, results.DeleteResult):
+        if result.deleted_count == 1:
+            return (1, 'Word {word} deleted successfully')
+
+    return (0, 'Word {word} not found')
+
+
+# get a reference to the collections
+try: 
+    attempted_words = intera_practice_db['attempted_words']
+except errors.CollectionInvalid as err:
+    print(err)
+
+
+def create_attempted_word_entry(word, user_id, classification, correct):
+    word_entry = {'word': word, 'user_id': user_id, 'classification': classification, 'correct': correct, 'date_created': datetime.now()}
+    result = attempted_words.find_one_and_replace(filter={ '$and': {'word': word, 'user_id': user_id}}, replacement=word_entry, upsert=True)
+
+    if result is not None:
+        return (1, 'Word attempt {word} created successfully')
+
+    return (0, 'Error creating word attempt {word}')
+
+
+def get_attempted_words(user_id):
+    words = attempted_words.find({'user_id': user_id}).sort('date_created', -1)
+
+    if words is None:
+        return (0, 'No words found', None)
+    
+    return (1, 'success', list(words))
+
+
+def get_attempted_word(word, user_id):
+    word_entry = attempted_words.find({'$and': {'word': word, 'user_id': user_id}}).sort('date_created', -1)
+
+    if word_entry.count() == 0:
+        return (0, 'Word not found', None)
+    
+    return (1, 'success', list(word_entry)[0])
+
+
+def delete_attempted_word(word, user_id):
+    result = attempted_words.delete_one({'word': word, 'user_id': user_id})
+
+    if isinstance(result, results.DeleteResult):
+        if result.deleted_count == 1:
+            return (1, 'Word attempt {word} deleted successfully')
+    
+    return (0, 'Word attempt {word} not found')
--- app/rooms/controller.py
+++ app/rooms/controller.py
@@ -1,5 +1,5 @@
 from config import Database
-from pymongo import errors
+from pymongo import errors, results
 from bson.json_util import dumps
 from os import environ as env
 from dotenv import find_dotenv, load_dotenv
@@ -33,12 +33,19 @@ def create_room(room_id, user_id, host_type):
     room = {'room_id': room_id, 'users': [user_id], 'host_type': host_type, 'date_created': datetime.now(), 'active': True, 'messages': []}
     
     try:
-        rooms.insert_one(room)
+        result = rooms.insert_one(room)
     except errors.DuplicateKeyError as err:
         return (err.code, err._message)
     except errors.PyMongoError as err:
         return (0, err._message)
-    return (1, room_id)
+
+    if isinstance(result, results.InsertOneResult):
+        if result.inserted_id is not None:
+            return (1, 'Room successfully created')
+        else:
+            return (0, 'Room not created')
+
+    return (0, 'Error creating room')
 
 def validate_room(room_id, user_id):
     room = rooms.find_one({'room_id': room_id})
@@ -87,10 +94,10 @@ def get_room(room_id):
     try:
         room = rooms.find_one({'room_id': room_id})
     except errors.PyMongoError as err:
-        return (0, err._message)
+        return (0, err._message, None)
     
     if room is None:
-        return (0, 'Room does not exist')
+        return (0, 'Room does not exist', None)
     
     return (1, 'success', room)
 
@@ -119,10 +126,10 @@ def add_room_messages(room_id, message):
     try:
         room = rooms.find_one_and_update({'room_id': room_id}, {'$push': {'messages': message}})
     except errors.PyMongoError as err:
-        return (0, err._message)
+        return (0, err._message, None)
     
     if room is None:
-        return (0, 'Room does not exist')
+        return (0, 'Room does not exist', None)
 
     room_messages = room['messages']
 
@@ -132,13 +139,13 @@ def get_all_rooms():
     try:
         all_rooms = rooms.find()
     except errors.PyMongoError as err:
-        return (0, err._message)
+        return (0, err._message, None)
     except errors.DocumentTooLarge as err:
-        return (0, str(err))
+        return (0, str(err), None)
     except Exception as err:
-        return (0, str(err))
+        return (0, str(err), None)
     
-    return (1, list(all_rooms))
+    return (1, 'success', list(all_rooms))
 
 def get_all_rooms_by_id(user_id):
     try:
