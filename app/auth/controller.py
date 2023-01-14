5b1fb1f88c56e9c69231aa9f7491618f2ce2cd39
NichHarris <harris.nicholas1998@gmail.com>
2023-01-14 17:12:26 -0500

    update auth

--- app/auth/controller.py
+++ app/auth/controller.py
@@ -5,11 +5,12 @@ from six.moves.urllib.request import urlopen
 from flask import _request_ctx_stack, request
 from jose import jwt
 import json
+import http.client
 
 load_dotenv(find_dotenv())
 AUTH0_CLIENT_SECRET = env.get('AUTH0_CLIENT_SECRET')
 
-def get_auth_token(request):
+def get_auth_token(request=request):
     auth = request.headers.get('Authorization', None)
     token = auth.split(' ')[1] if auth else None
 
@@ -31,9 +32,43 @@ def get_rsa_key(token, jwks):
 
     return rsa_key
 
+def get_management_token():
+    conn = http.client.HTTPSConnection("")
+
+    CLIENT_ID = env.get('AUTH0_CLIENT_ID')
+    CLIENT_SECRET = env.get('AUTH0_CLIENT_SECRET')
+    DOMAIN = env.get('AUTH0_ISSUER_BASE_URL')
+
+
+    payload = f"grant_type=client_credentials&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&audience=https://{DOMAIN}/api/v2/"
+
+    headers = { 'content-type': "application/json" }
+
+    conn.request("POST", f"https://{DOMAIN}/oauth/token", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    return data.decode("utf-8")
+
+def management_api_user():
+
+    conn = http.client.HTTPSConnection("")
+    yourMgmtApiAccessToken = ''
+    headers = { 'authorization': f"Bearer {yourMgmtApiAccessToken}" }
+    email = ''
+    conn.request("GET", f"/YOUR_DOMAIN/api/v2/users?q=email={email}&search_engine=v3", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
+
 def decode_jwt(token):
     BASE_URL = env.get('AUTH0_ISSUER_BASE_URL')
     jsonurl = urlopen(f'https://{BASE_URL}/.well-known/jwks.json')
+
+    # JSON Web Token key set
     jwks = json.loads(jsonurl.read())
 
     rsa_key = get_rsa_key(token, jwks)
@@ -47,6 +82,10 @@ def decode_jwt(token):
             issuer=f'https://{BASE_URL}/'
         )
 
+        AUTH0_BASE_URL = env.get('AUTH0_ISSUER_BASE_URL')
+        if payload['iss'] != f'https://{AUTH0_BASE_URL}/':
+            raise Exception('401 - Unauthorized Invalid issuer')
+
         return payload
     return None
 
@@ -57,6 +96,8 @@ def requires_auth(func):
     @wraps(func)
     def decorated(*args, **kwargs):
         token = get_auth_token(request)
+        if not token:
+            raise Exception('401 - Authorization header is expected')
         payload = decode_jwt(token)
         if payload:
             _request_ctx_stack.top.current_user = payload
