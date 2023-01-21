10cb0f2ef6e77725d06734d1dc8bcd1cf2633961
NichHarris <harris.nicholas1998@gmail.com>
2023-01-21 14:51:39 -0500

    add template

--- app/sockets/routes.py
+++ app/sockets/routes.py
@@ -23,3 +23,43 @@ def disconnect():
     return Response('Client disconnected!!!')
 
 # TODO: Add other routes
+
+# @socket_io.on('create_room', namespace=NAMESPACE)
+# def create_room(data):
+#     pass
+
+
+# @socket_io.on('join_room', namespace=NAMESPACE)
+# def join_room(data):
+#     room_id = data['room_id']
+#     user_id = data['user_id']
+#     join_room(room_id)
+
+
+# @socket_io.on('leave_room', namespace=NAMESPACE)
+# def leave_room(data):
+#     room_id = data['room_id']
+#     user_id = data['user_id']
+
+#     users = rooms_api.get_room_users(room_id)
+
+#     if users == None:
+#         return
+
+#     # check if user is host
+#     if user_id == users[0]:
+#         # close room
+#         socketio.close_room(room_id, namespace=NAMESPACE)
+
+#         for user in users:
+#             socketio.disconnect(user, namespace=NAMESPACE)
+#     else:
+#         socketio.leave_room(user_id, room_id, namespace=NAMESPACE)
+#         socketio.disconnect(user_id, namespace=NAMESPACE)
+
+
+# @socket_io.on('send_message', namespace=NAMESPACE)
+# def send_message(data):
+#     room_id = data['room_id']
+#     user_id = data['user_id']
+#     message = data['message']
