41895bc656789be805613fbd713f9ed1bd075540
NichHarris <harris.nicholas1998@gmail.com>
2023-01-28 22:51:31 -0500

    ws changes

--- app/sockets/routes.py
+++ app/sockets/routes.py
@@ -1,8 +1,12 @@
 
 from app import socket_io
-from flask_socketio import emit, join_room, leave_room, close_room, rooms, disconnect
+from flask_socketio import emit, join_room, leave_room, close_room, rooms, disconnect, send
 from flask import request, Response
 from flask_cors import cross_origin
+
+from app.rooms import controller as rooms_api
+from app.transcripts import controller as transcripts_api
+from app.auth import controller as auth
   
 @socket_io.on_error_default
 @cross_origin(headers=["Origin", "Content-Type", "Authorization", "Accept"], supports_credentials=True)
@@ -22,7 +26,33 @@ def disconnect():
     print('Client disconnected!!!')
     return Response('Client disconnected!!!')
 
-# TODO: Add other routes
+
+@socket_io.on('join')
+@cross_origin(headers=["Origin", "Content-Type", "Authorization", "Accept"], supports_credentials=True)
+def join(data):    
+
+    # Need to figure out if we can send the auth header in the socket request
+    user_info = auth.decode_jwt(data['authorization'])
+    if user_info is None:
+        return Response('User not authenticated', status=401)
+    
+    user_id = user_info['nickname']
+
+    room_id = data['room_id']
+
+    join_room(room_id)
+    print(f'{user_id} has entered the room id: {room_id}.')
+    send(f'{user_id} has entered the room id: {room_id}.', to=room_id)
+    return Response(f'{user_id} has entered the room id: {room_id}.')
+
+@socket_io.on('leave')
+@cross_origin(headers=["Origin", "Content-Type", "Authorization", "Accept"], supports_credentials=True)
+def leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
 
 # @socket_io.on('create_room', namespace=NAMESPACE)
 # def create_room(data):
