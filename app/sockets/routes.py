8b1dab2b73756b8a515966233c9584be921db9df
mattsklivas <msklivas@live.com>
2023-02-01 21:40:31 -0500

    testing

--- app/sockets/routes.py
+++ app/sockets/routes.py
@@ -64,6 +64,26 @@ def mutate(data):
     emit('mutate', {'id': request.sid, 'roomID': data['roomID']}, broadcast=True, to_room=data['roomID'], skip_sid=request.sid)
     return Response('OK')
 
+
+@socket_io.on('join')
+def join(message):
+    username = message['username']
+    room = message['room']
+    join_room(room)
+    print('RoomEvent: {} has joined the room {}\n'.format(username, room))
+    emit('ready', {username: username}, to=room, skip_sid=request.sid)
+    return Response('OK')
+
+
+@socket_io.on('data')
+def transfer_data(message):
+    username = message['username']
+    room = message['room']
+    data = message['data']
+    print('DataEvent: {} has sent the data:\n {}\n'.format(username, data))
+    emit('data', data, to=room, skip_sid=request.sid)
+    return Response('OK')
+
 # TODO: Add other routes
 
 # @socket_io.on('create_room', namespace=NAMESPACE)
